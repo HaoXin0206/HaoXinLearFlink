050f3399ff9c7f5bf1c6a62605a520ca7a379bf1
HaoXin0206 <1227462120@qq.com>
2019-06-25 21:55:40 +0800

    实时代码

--- src/main/Scala/HaoXin/LearFlink/Core/ReadHdfsFile.scala
+++ src/main/Scala/HaoXin/LearFlink/Core/ReadHdfsFile.scala
@@ -1,23 +1,22 @@
 package HaoXin.LearFlink.Core
 
 import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.scala._
 import org.apache.flink.runtime.rest.messages.job.metrics.MetricsAggregationParameter._
 
 object ReadHdfsFile {
   def main(args: Array[String]): Unit = {
-    val env=ExecutionEnvironment.getExecutionEnvironment
-    val path="/haoxin/data"
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val path = "/haoxin/data"
     val data = env.readTextFile(s"hdfs://haoxin01:8020${path}")
       .flatMap(_.split(""))
-      .map((_,1))
+      .map((_, 1))
       .groupBy(0)
       .sum(1)
-
-      data.setParallelism(1)
-      .writeAsCsv("hdfs://haoxin01:8020/haoxin/flink1")
-
-    env.execute()
+      .sortPartition(1,Order.ASCENDING)
+      .setParallelism(1)
+      .print()
 
   }
 
