6183b6b875bda38c6740a885c4396185816f0dbf
HaoXin0206 <1227462120@qq.com>
2019-11-30 14:25:19 +0800

    添加对parquet文件读取的支持

--- src/main/Scala/LocalFile/ReadFile.scala
+++ src/main/Scala/LocalFile/ReadFile.scala
@@ -1,7 +1,13 @@
 package LocalFile
 
+import org.apache.flink.streaming.api.scala._
+import org.apache.parquet.hadoop.ParquetFileReader
+
+
 object ReadFile {
   def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
 
   }
 
